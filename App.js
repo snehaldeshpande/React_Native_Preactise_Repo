a4532224f41dca673909d6320790d33f5c0547c8
unknown <snehaldeshpande1010@gmail.com>
2020-04-30 15:33:40 +0530

    first commmit

---
+++ App.js
@@ -0,0 +1,18 @@
+
+
+import * as React from 'react';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
+
+
+export default class App extends React.Component {
+
+ 
+  render() {
+    return (
+      <View style={{padding:30}}>
+        <Text>Welcome To React Native</Text>
+        </View>
+    );
+  }
+}
+      
